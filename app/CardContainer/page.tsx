f3ea67a865dfb3d759ffb2cf3c990128ef79851f
David Beshensky <beshenskydave@gmail.com>
2023-05-17 14:14:35 -0500

    new addition

---
+++ app/CardContainer/page.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+interface CardProps {
+  title: string;
+  content: string;
+}
+
+const Card: React.FC<CardProps> = ({ title, content }) => (
+  <div
+    className="border-2 p-2 m-2 rounded-lg bg-blue-500 hover:bg-green-500 shadow-2xl transition duration-300 ease-in-out transform hover:scale-105 hover:opacity-75"
+  >
+    <div className="flex flex-col items-center justify-center">
+      <h3 className="text-center">{title}</h3>
+      <p>{content}</p>
+    </div>
+  </div>
+);
+
+const CardContainer: React.FC = () => (
+  <div className="flex justify-center items-center h-screen bg-green-500">
+    <Card title="Card 1" content="This is the content of Card 1." />
+    <Card title="Card 2" content="This is the content of Card 2." />
+    <Card title="Card 3" content="This is the content of Card 3." />
+  </div>
+);
+
+export default CardContainer;
