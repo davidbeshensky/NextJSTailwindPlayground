1b8ea51f3b9a1c8be4d12010e65ea6408f0b61ba
David Beshensky <beshenskydave@gmail.com>
2024-07-09 15:26:13 -0500

    adding some notes

--- app/LeetCodeHistory/Arrays/page.tsx
+++ app/LeetCodeHistory/Arrays/page.tsx
@@ -32,6 +32,53 @@ const LeetCodeProblem = () => {
         or you can manually add using myset.add()
       </p>
     </div>
+    <div>
+        <h1>understanding hash maps</h1>
+        <pre className="p-4">
+        <code>
+          {`function isAnagram(s,t,map = new Map()) {
+            const isEqual = s.length === t.length;
+            if (isEqual) return false;
+
+            addFrequency(s, map);
+            subtractFrequency(t, map);
+
+            return checkFrequency(map);
+            };
+
+            const addFrequency = (str, map) => {
+                for (const char of str) {
+                    const count = (map.get(char) || 0) + 1;
+
+                    map.set(char, count);
+                }
+            }
+
+            const subtractFrequency = (str, map) => {
+                for (const char of str) {
+                    if (!map.has(char)) continue;
+
+                    const count = map.get(char) - 1;
+                    
+                    map.set(char, count);
+                }
+            };
+
+            const checkFrequency = (map) => {
+                for (const [char, count] of map) {
+                    count isEmpty = count === 0;
+                    if (!isEmpty) return false;
+                }
+                return true;
+            };`}
+        </code>
+      </pre>   
+    <div>
+    Explaining the solution: first we check if the lengths are equal to return that specific case quickly
+    addFrequency s, map; this function iterates over each character in string s and updates the hash map with the frequency of each character.
+    </div>
+    </div>
+
     </div>
   );
 };
